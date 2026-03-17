4f1c40d6064b9bdf0241651349327e69e1419c69
asp <asp@aptech.com>
2026-03-17 09:53:23 +0500

    added Subscriber Model Class in models

---
+++ Models/Subscriber.cs
@@ -0,0 +1,17 @@
+﻿namespace Quote_genarator.Models
+{
+    public class Subscriber
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsSubscribed { get; set; } = false;
+
+        public bool IsConfirmed { get; set; } = false;
+
+        public string ConfirmationToken { get; set; }
+
+        public DateTime SubscribedAt { get; set; } = DateTime.UtcNow;
+    }
+}
